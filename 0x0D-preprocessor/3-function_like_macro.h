c39f9f231da2c83d8254f2944276c2555bda99c2
VincentAdjei <nanaquarme95@gmail.com>
2023-04-17 21:13:29 +0000

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+  * main - function like macro ABS(x)
+  * Computes the absolute value of a number x
+  */
+
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#ifndef SUM_H
+#define SUM_H
+/**
+  * main -  function-like macro SUM(x, y)
+  * computes the sum of the numbers x and y
+  * x the first number to add
+  *
+  * y the second number to add
+  * @return the sum of x and y
+  */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif /*SUM_H*/
