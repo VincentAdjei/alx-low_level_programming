aed38d6e79f371860a04bd081aae4a3d70cfc6c8
VincentAdjei <nanaquarme95@gmail.com>
2023-05-08 22:36:49 +0000

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file with the given text content
+ * @filename: the name of the file to create
+ * @text_content: the text content to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_descriptor, write_status, text_length = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (text_length = 0; text_content[text_length];)
+			text_length++;
+	}
+
+	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	 write_status = write(file_descriptor, text_content, text_length);
+
+	if (file_descriptor == -1 || write_status == -1)
+		return (-1);
+
+	close(file_descriptor);
+
+	return (1);
+}
