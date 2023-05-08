657a2aab091659654812884c6c0d061e3bac0a12
VincentAdjei <nanaquarme95@gmail.com>
2023-05-08 20:45:04 +0000

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads a text file and prints it to POSIX stdout
+ * @filename: pointer to the name of the file
+ * @letters: number of letters the function should read and print
+ *
+ * Return: actual number of bytes the function can read and print,
+ * or 0 if the function fails or filename is NULL
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int file_descriptor, read_result, write_result;
+	char *buffer;
+
+	if (filename == NULL)
+	return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	return (0);
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+	{
+	free(buffer);
+	return (0);
+}
+	read_result = read(file_descriptor, buffer, letters);
+	if (read_result == -1)
+{
+	free(buffer);
+	close(file_descriptor);
+	return (0);
+}
+	write_result = write(STDOUT_FILENO, buffer, read_result);
+	if (write_result == -1 || write_result != read_result)
+{
+	free(buffer);
+	close(file_descriptor);
+	return (0);
+}
+	free(buffer);
+
+	close(file_descriptor);
+
+	return (write_result);
+}
